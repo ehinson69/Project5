9a86fe5aa42b78d8da918d066aa365d4a5c96d13
Elizabeth Hinson <elizabeth.hinson@verizon>
2019-08-05 11:37:30 -0500

    final

--- README.md
+++ README.md
@@ -5,7 +5,9 @@ General info
 My project is a Public API Requests. I'm building an application for a fictional company called Awesome Startup, a distributed company with remote employees working all over the world. They need a smart way for employees to share contact information with each other. I will grab information for 12 random employees and use their data to build a prototype for their directory, using the Random User Generator API. 
 
 Extra Credit
-
+Display color changed of employees' cards.
+Gives the modal a previous and next functionality. 
+Spent too much time trying to get the eventListener to happen for the extra credit, so at this point I chose to move on and will come back.
 
 Status
 Project is: finished
--- js/scripts.js
+++ js/scripts.js
@@ -2,136 +2,118 @@
 //**************************Project 5 - Public API Requests******************************/
 //**************************by Elizabeth Hinson******************************************/
 
-let employees = [];
-let cardHTML = '';
-//Grabbed the example provided.
+//Grabbed the example provided.	
 $.ajax({
-    url: 'https://randomuser.me/api/?results=12&nat=us',
-    dataType: 'json',
-    success: function (data) {
-        employees = data.results;
-    }
+        url: 'https://randomuser.me/api/?results=12&nat=us',
+        dataType: 'json',
+        success: function (data) {
+            employees = data.results;
+        }
 })
 
-    .done(() => {
-        employees.forEach((employee) => {
-            cardHTML += `<div class="card">
+    //set variable for user results
+    //loops through each employee to get their information
+    .then(function (users) {
+        data = users.results;
+        data.forEach(employees => {
+            // variables for employees information
+            const image = employees.picture.medium;
+            const firstName = employees.name.first;
+            const lastName = employees.name.last;
+            const email = employees.email;
+            const city = employees.location.city;
+            const state = employees.location.state;
+
+            //create variable for employee profile and set it to the div card
+            const profile = `
+            <div class="card">
                 <div class="card-img-container">
-                    <img class="card-img" src="${employee.picture.medium}" alt="profile picture">
+                    <img class="card-img" src="${image}" alt="profile picture">
                 </div>
                 <div class="card-info-container">
-                    <h3 id="name" class="card-name cap">${employee.name.first} ${employee.name.last}</h3>
-                    <p class="card-text">${employee.email}</p>
-                    <p class="card-text cap">${employee.location.city}, ${employee.location.state}</p>
+                    <h3 id="name" class="card-name cap">${firstName} ${lastName}</h3>
+                    <p class="card-text">${email}</p>
+                    <p class="card-text cap">${city}, ${state}</p>
                 </div>
             </div>`
+            // append profile to gallery id
+            $("#gallery").append(profile);
         })
-        $(".gallery").append(cardHTML);
-   
-            
-        $(".modal-container").show();
-        $("#modal-close-btn").on("click", function() {
-            $(".modal-container").hide();
-        })
-
-        //Add a click event for the modal prev
-        //Decrement the currentIndex
-        //If the currentIndex is less than zero, reset it to zero
-        $("#modal-prev").on("click", function() {
-            currentIndex = parseInt(currentIndex) - 1;
-            if(currentIndex < 0) {
-                currentIndex = 0;
-            };
-            populateModalPopup(currentIndex);
-        })
-
-        // Add a click event for the modal next
-        // Increment the currentIndex
-        // If currentIndex is greater than 11, reset it to 11 i.e.
-        // the maximum number of random users
-        $("#modal-next").on("click", function() {
-            currentIndex = parseInt(currentIndex) + 1;
-            if (currentIndex > 11) {
-                currentIndex = 11;
-            };
-            populateModalPopup(currentIndex);
-        })
-
     });
 
-        // Gives the modal a previous and next functionality. Extra Credit.
-        function createModalBtnContainer() { 
-            let modalBtnContainer=
-            `<div class="modal-btn-container">
-                        <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
-                        <button type="button" id="modal-next" class="modal-next btn">Next</button>
-                    </div>`;
-            $(".modal-container").append(modalBtnContainer);
-            $(".modal-btn-container").show();
-            $(".modal-container").hide();
+    //variable for other detailed information that will appear when employees are selected
+    function display(i) {
+        const image = data[i].picture.large;
+        const firstName = data[i].name.first;
+        const lastName = data[i].name.last;
+        const email = data[i].email;
+        const city = data[i].location.city.toUpperCase();
+        const state = data[i].location.state.toUpperCase();
+        const phone = data[i].phone;
+        const street = data[i].location.street.toUpperCase();
+        const birthMonth = data[i].dob.date.slice(5, 7);
+        const birthDate = data[i].dob.date.slice(8, 10);
+        const birthYear = data[i].dob.date.slice(0, 4);
+
+    //adds html element of information to the popup window
+    let modal = 
+            `<div class="modal-container">
+                <div class="modal">
+                <button type="button" id ="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+                <div class="modal-info-container">
+                    <img class ="modal-img" src="${image}" alt="profile picture">
+                    <h3 id="name" class="modal-name cap">${firstName} ${lastName}</h3> 
+                    <p class="modal-text">${email}</p> 
+                    <p class="modal-text cap">${city}</p> 
+                    <hr>
+                    <p class="modal-text">${phone}</p> 
+                    <p class="modal-text">${street}, ${city}, ${state} 97204</p> 
+                    <p class="modal-text">Birthday: ${birthMonth}/${birthDate}/${birthYear}</p> 
+                </div>
+                <div class="modal-btn-container">
+                    <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+                    <button type="button" id="modal-next" class="modal-next btn">Next</button>
+                </div>
+            </div>`
+
+    //Gives the modal a previous and next functionality. Extra Credit.
+    let modalBtnContainer =
+        `<div class="modal-btn-container">
+            <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+            <button type="button" id="modal-next" class="modal-next btn">Next</button>
+        </div>`;
+    $(".modal-container").append(modalBtnContainer);
+    $(".modal-btn-container").show();
+    $(".modal-container").hide();
+    $("body").append(modal);
+
+    //appends html to popup window
+    $("#modal-close-btn").on("click", function() {
+        $(".modal-container").remove();
+    })
+
+    $("#modal-next").on("click", function(index) {
+        let next;
+        if(index == employees.length - 1) {
+            next = 0
+        } else {
+            next = index + 1;
         }
+    })
 
-    //Function to populate the modal popup
-    function populateModalPopup(contacts) {
-        $(".modal-img")[0].src = employee[contacts].picture.medium;
-        $("#name").text(employee[contacts].name.first + " " + employee[contacts].name.last);
-        $(".modal-text")[0].innerText = employee[contacts].email;
-        $(".modal-text")[1].innerText = employee[contacts].location.city;
-        $(".modal-text")[2].innerText = employee[contacts].cellular;
-        $(".modal-text")[3].innerText = employee[contacts].location.street + ", " +
-            employee[contacts].location.city + ", " +
-            employee[contacts].location.state + " " +
-            employee[contacts].location.postalcode;
+    // Add a click event for the modal next
+    $("#modal-prev").on("click", function(index) {
+        let prev;
+        if(index == employees.length + 1) {
+            prev = 0
+        } else {
+            prev = index - 1;
+        }
+    })
+}
 
-        let DOB = employee[contacts].dob.date.split("T");
-        let DOBArr = DOB[0].split("-");
-        let year = DOBArr[0];
-        let month = DOBArr[1];
-        let day = DOBArr[2];
-        $(".modal-text")[4].innerText = "Birthday: " + month + "/" + day + "/" + year;
-    }
-
-    employees.forEach((employee) => {
-        employeesModal += `<div class="modal-container">
-                            <div class="modal">
-                                <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-                            </div>
-                            <div class="modal-info-container">
-                                <img class="modal-img" src="${employee.picture.medium}" alt="profile picture"></img>
-                                    <h3 id="name" class="modal-name cap">${employee.name.first} ${employee.name.last}</h3>
-                                    <p class="modal-text">${employee.email}</p>
-                                    <p class="modal-text cap">${employee.location.city}</p>
-                                    <hr>
-                                    <p class="modal-text">(555) 555-5555</p>
-                                    <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
-                                    <p class="modal-text">Birthday: 10/21/2015</p>
-                            </div>
-                        </div>`
-    });
-
-    $(".body").append(employeeModal);
-    gallery.after(modalPopup);
-    
-
-    //Gives the search box functionality.
-    function createSearch() { 
-        let search =
-            `<form action="#" method="get">
-                <input type="search" id="search-input" class="search-input" placeholder="Search...">
-                <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
-            </form>`;
-        $(".search-container").append(search);
-    }
-
-    //listen for clicks on a card; use name to get detailed data
-        // Add a click event for the search submit button
-        $("#search-submit").on("click", function() {
-            let searchVal = $("#search-input").val().toLowerCase();
-            $(".card").hide();
-            for(let s = 0; s < $(".card").length; s++) {
-                if($("#name" + s)[0].innerText.toLowerCase().indexOf(searchVal) != -1) {
-                    $(".card")[s].style.display = "block";
-                }
-            }
-        })
-   
+    //when clicked on employee card, modal container appears
+    $('#gallery').on("click", ".card", function () {
+        i = ($(this).index())
+        display(i);
+    })
